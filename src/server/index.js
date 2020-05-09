422d29de22ca4a6b27bb5fb08f7338bd2b8a53ae
Haithm EL-Watany <h.elwatany@gmail.com>
2020-05-09 06:30:28 +0200

    Setting up the server

--- src/server/index.js
+++ src/server/index.js
@@ -1,33 +1,72 @@
-const dotenv = require('dotenv');
-dotenv.config();
+// Set DotEnv
+const dotenv = require('dotenv')
+dotenv.config()
 
-let path = require('path')
-const express = require('express')
+// Set Path
+const path = require('path')
+
+// Set IP
+const ip = require('ip')
+
+// Setup API mockup
 const mockAPIResponse = require('./mockAPI.js')
-let aylien = require('aylien_textapi')
 
-const app = express()
-
-app.use(express.static('dist'))
-
-// set aylien API credentias
+// Setup Aylien SDK
+const aylien = require('aylien_textapi')
 let textapi = new aylien({
     application_id: process.env.API_ID,
     application_key: process.env.API_KEY
 })
 
-console.log(__dirname)
 
+/**
+ * INIT SERVER
+ */
+// Require Express to run server and routes
+const express = require('express')
+
+// Start up an instance of app
+const app = express()
+
+/** Middlewares **/
+// Configure body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors')
+app.use(cors())
+
+// Initialize the dist directory
+app.use(express.static('dist'))
+
+// Setup Server Port
+const port = 3000
+
+// Setup Server
+const server = app.listen(port, listening)
+
+
+/**
+ * ROUTES
+ */
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
-
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+
+/**
+ * CALLBACK FUNCTIONS
+ */
+// Server callback debug function
+function listening() {
+    console.log('Website running at:')
+    console.log(`  - Local: http://localhost:${port}`)
+    console.log(`  - Network: http://${ip.address()}:${port}`)
+}
